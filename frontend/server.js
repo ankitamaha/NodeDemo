07da539f350ffa6a71193f71af613c30a188c210
Jared Barboza <jared.m.barboza@gmail.com>
2016-05-19 17:28:59 -0500

    update frontend code

--- frontend/server.js
+++ frontend/server.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const express = require('express');
+const request = require('request');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const BACKEND = process.env.BACKEND || 'localhost';
 
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  request(`http://${BACKEND}:5000/api`, (err, r, body) => {
+    if (!err && res.statusCode == 200) {
+      res.send(body);
+    }
+  });
 });
 
+
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log(`Running on http://localhost:${PORT}`);
