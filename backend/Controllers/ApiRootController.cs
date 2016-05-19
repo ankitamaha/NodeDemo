90606fe30a0f96df94d58ec5f157ee03843f026b
Jared Barboza <jared.m.barboza@gmail.com>
2016-05-19 15:59:47 -0500

    Create ApiRootController.cs

---
+++ backend/Controllers/ApiRootController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApplication {
+    [Route("/api")]
+    public class ApiRootController : Controller {
+        [HttpGet]
+        public object Get() {
+            return new { 
+                Users = "/api/users"
+            };
+        }
+    }
+}
